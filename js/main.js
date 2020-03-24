98f8cca7a11147011e79da15ea70d0c271a551a3
Vitaly Gachkovsky <wavelolmu@gmail.com>
2020-03-24 18:38:26 +0300

    WIP

--- js/main.js
+++ js/main.js
@@ -8,6 +8,30 @@ window.onload = function calcDelay() {
   }
 }
 
+function hideButton() {
+  document.querySelector('#starter-tarot-button').style.display = 'none'
+}
+function activateHover() {
+  document.querySelector('#hover-disactivator').style.display = 'none'
+}
+function moveStars() {
+  document.querySelector('#starter-star4').style.opacity = '0';
+  function displayNone() {
+    document.querySelector('#starter-star4').style.display = 'none';
+  }
+  setTimeout(displayNone, 400);
+  document.querySelector('#starter-star5').style.right = '1122px';
+  document.querySelector('#starter-star5').style.top = '197.6vh';
+  document.querySelector('#starter-star6').style.right = '690px';
+  document.querySelector('#starter-star6').style.top = '252vh';
+  document.querySelector('#starter-star6').style.height = '65px';
+  document.querySelector('#starter-star7').style.left = '510px';
+  document.querySelector('#starter-star7').style.top = '192vh';
+  document.querySelector('#starter-star8').style.top = '174vh';
+  document.querySelector('#starter-star8').style.height = '50px';
+  document.querySelector('#starter-star9').style.opacity = '1';
+}
+
 function futureTelling() {
   for (var i = 1; i < 6; i++) {
     var tarotCard = document.querySelector('#starter-tarot-card' + i);
@@ -15,8 +39,9 @@ function futureTelling() {
   }
   document.querySelector('#starter-tarot-boyfriend').style.transform = 'none';
   document.querySelector('#starter-tarot-button').style.opacity = '0';
-  function hideButton() {
-    document.querySelector('#starter-tarot-button').style.display = 'none'
-  }
+  document.querySelector('#starter-tarot-moon').style.opacity = '0';
+  document.querySelector('#starter-tarot-dihotomia').style.opacity = '0';
   setTimeout(hideButton, 400);
+  setTimeout(activateHover, 500);
+  moveStars();
 }
